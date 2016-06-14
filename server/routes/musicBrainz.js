8412d17da98fa2e5596e10dc8a6b5862eb8d5bf7
Zach Kusz <zach@Zachs-MacBook-Pro.local>
2016-06-14 17:28:07 -0500

    adds server caching, adds styling

--- server/routes/musicBrainz.js
+++ server/routes/musicBrainz.js
@@ -3,17 +3,22 @@ var router = express.Router();
 var passport = require('passport'); //may not need
 var path = require('path');
 var request = require('request');
-var urlencode = require('urlencode');
+  cachedRequest = require('cached-request')(request)
+  cacheDirectory = path.join(__dirname, "../../tmp/cache");
+  cachedRequest.setCacheDirectory(cacheDirectory);
+var urlencode = require('urlencode'); //probs uninstall
+
 
 router.get('/today', function (req, res) {
   var today = new Date().toISOString().slice(0,10);
-  var req = {
+  var options = {
     url: 'http://musicbrainz.org/ws/2/release/?query=date:' + today + '&limit=100',
     headers: {
       'User-Agent': 'MusicalForecaster/version-inDevelopment (zskusz@gmail.com)'
-    }
+    },
+    ttl: 60000
   };
-  request(req, function(err, response, body) { //changed games to body
+  cachedRequest(options, function(err, response, body) { //changed to cached
     if (err) {
       res.sendStatus(500);
       return;
@@ -43,13 +48,14 @@ router.get('/albums/:artist', function (req, res) {
   var artist = req.params.artist;
   //var encodedReq = urlencode('AND arid:' + artist);
   //console.log(encodedReq);
-  var req = {
+  var options = {
     url: 'http://musicbrainz.org/ws/2/release?artist=' + artist + '&status=official&type=album&limit=100&inc=artist-credits',
     headers: {
       'User-Agent': 'MusicalForecaster/version-inDevelopment (zskusz@gmail.com)'
-    }
+    },
+    ttl: 60000
   };
-  request(req, function(err, response, body) {
+  cachedRequest(options, function(err, response, body) {
     if (err) {
       res.sendStatus(500);
       return;
